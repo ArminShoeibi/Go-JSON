b73798b92dbcdfc1ac852e9ee965a862bbfb5910
a.shoeibi <a.shoeibi@DESKTOP-TPOSAII>
2021-06-25 16:03:17 +0430

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	var me string = `{"FirstName":"Armin", "LastName":"Shoeibi"}`
+	fmt.Println(me)
+
+	var p Person
+	err := json.Unmarshal([]byte(me), &p)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(p)
+}
